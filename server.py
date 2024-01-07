d89bfffbf1bddfbe3eeb3895c9b3835af2167bff
Belena <helena082003@gmail.com>
2024-01-06 21:20:56 -0300

    config servidor

--- server.py
+++ server.py
@@ -1,25 +1,56 @@
 import socket
+import threading
 
 #constantes para armazenar IP  e número da porta
 HOST = 'localhost'
 PORT = 50000
 
 #criação do objeto socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((HOST, PORT))
+clients = []
+nicknames = []
 
 # servidor em modo de escuta
-s.listen()
+server.listen()
 print('Aguardando conexão de um cliente')
 
-conexao, endereco = s.accept()
-print('Conectado em', endereco)
 
-####troca de mensagens###
-while True:
-    data = conexao.recv(1024)
-    if not data: #quando não houver nada na mensagem
-        print('Fechando a conexão')
-        conexao.close()
-        break
-    conexao.sendall(data) #envia de volta pro cliente
+def broadcast(mensagem):
+    for client in clients:
+        client.sendall(mensagem)
+
+
+def handle_connections(client):
+    while True:
+        data = client.recv(1024)
+        #envia mensagem para todos os clientes
+        broadcast(data)
+        if not data:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} saiu da sala!'.encode('utf-8'))
+            nicknames.remove(nickname)
+            break
+
+
+###receber conexão dos clientes###
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'conexão estabelecida com {address}')
+        client.send('nickname?'.encode('utf-8'))
+        nickname = client.recv(1024)
+        clients.append(client)
+        nicknames.append(nickname)
+        print(f'O nome de usuário do cliente é {nickname}'.encode('utf-8'))
+        broadcast(f'{nickname} entrou no chat!'.encode('utf-8'))
+        client.send('Você está conectado!'.encode('utf-8'))
+        ###executar tarefas paralelas
+        thread = threading.Thread(target= handle_connections, args=(client,))
+        thread.start()
+
+if __name__ == "__main__":
+    receive()
