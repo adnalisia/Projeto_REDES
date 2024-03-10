cab5c5e109203b8fc29e0cd48fd8a862ed81e774
Maria Antônia <131059210+mams5@users.noreply.github.com>
2024-03-10 16:54:27 -0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,90 @@
+import socket
+
+# Calcula complemento de 1
+def complement(n, size):
+    comp = n ^ ((1 << size) - 1)
+    return '0b{0:0{1}b}'.format(comp, size)
+
+'''
+# Soma de verificação
+def checksum(data, size):
+    first_sum = bin(data)[2:].zfill(16)
+    if (len(first_sum) > 16):
+        first_sum = first_sum[1:17]
+        first_sum = bin(int(first_sum, 2) + 1)[2:].zfill(16)
+    second_sum = bin(int(first_sum, 2)+size)[2:].zfill(16)
+    if (len(second_sum) > 16):
+        second_sum = second_sum[1:17]
+        second_sum = bin(int(second_sum, 2) + 1)[2:].zfill(16)
+    checksum = complement(int(second_sum, 2), 16)[2:]
+    return int(checksum, 2)
+'''
+# Função que transforma bytes em bits
+def bytes_to_bits(data):
+    message_bits = bin(int.from_bytes(data, byteorder='big'))[2:]
+    return message_bits
+
+# Checksum bom
+
+def checksum(data, size):
+    bits = bytes_to_bits(data)
+    # Dividindo em 8 bits
+    bytes_parts_list = []   # cria uma lista para as partes em 8 bits
+    part_lenght = 8
+    # Loop para dividir a mensagem em partes de 8 bits
+    while bits:
+        byte_part = bits[0:part_lenght]
+        # Adiciona as partes na lista
+        bytes_parts_list.append(byte_part)
+
+    # Soma as partes
+    for bit_part in bytes_parts_list:
+        # soma cada parte com a seguinte
+        bits_sum = bin(int(bit_part, 2))[2:]
+        return bits_sum
+
+    # Adicionando o overflow
+    if len(bits_sum > part_lenght):
+        # Calcula os bits overflow
+        exceed = len(bits_sum) - part_lenght
+        # Soma os bits overflow ao resultado sem o overflow
+        new_sum = bin(int(bits_sum[0:exceed], 2)+int(bits_sum[exceed:], 2))[2:]
+    # Adiciona zeros à esquerda em caso de soma menor que 8 bits
+    if len(bits_sum) < part_lenght:
+        new_sum = '0' * (part_lenght - len(bits_sum)) + bits_sum
+
+    complement(new_sum)
+
+# Calculando o complemento de 1
+def complement_1(new_sum):
+    the_checksum = ''
+    for bit in new_sum:
+        # Troca os 1 por 0
+        if bit == '1':
+            the_checksum += '0'
+        # Troca os 0 por 1
+        else:
+            the_checksum += '1'
+    return the_checksum
+
+#função de enviar
+def rdt_send(data, seqnumb, receiver):
+    data = data.encode()
+    #primeiro fazendo o checksum dos dados
+    cks = checksum(data, len(data))
+    #criamos o pacot com o id, os dados e o checksum
+    sndpkt = [seqnumb, data, cks]
+    #envia o pacote pro destinatario
+    socket.sendto(sndpkt, receiver)
+
+
+#função de receber
+def rdt_rcv():
+    rcvpkt, sender = socket.recvfrom(1024)
+    seqnumb = rcvpkt[0]
+    data = rcvpkt[1]
+    cks = checksum(data)
+    if cks == rcvpkt[2]:
+        rdt_send('ACK', seqnumb, sender)
+    else:
+        rdt_send('NAK', seqnumb, sender)
