78a737f8c2c62e670fc8b6bfb5511216e2453759
Maria Antônia <131059210+mams5@users.noreply.github.com>
2024-03-11 13:03:37 -0300

    Função checksum corrigida, com nova checagem

--- functions.py
+++ functions.py
@@ -1,16 +1,19 @@
 import socket
 
 # Função checksum
-
 def checksum(data):
     # Transforma bytes em bits
     message_bits = bin(int.from_bytes(data, byteorder='big'))[2:]
     # Dividindo em 8 bits
-    bytes_parts_list = []   # cria uma lista para as partes em 8 bits
+    bytes_parts_list = []    # cria uma lista para as partes em 8 bits
     part_lenght = 8
+    j = 8
+    i = 0
     # Loop para dividir a mensagem em partes de 8 bits
-    while message_bits:
-        byte_part = message_bits[0:part_lenght]
+    while i < len(message_bits) :
+        byte_part = message_bits[i:j]
+        i = j
+        j = j + 8
         # Adiciona as partes na lista
         bytes_parts_list.append(byte_part)
 
@@ -18,10 +21,10 @@ def checksum(data):
     for bit_part in bytes_parts_list:
         # soma cada parte com a seguinte
         bits_sum = bin(int(bit_part, 2))[2:]
-        return bits_sum
+        #return bits_sum
 
     # Adicionando o overflow
-    if len(bits_sum > part_lenght):
+    if len(bits_sum) > part_lenght:
         # Calcula os bits overflow
         exceed = len(bits_sum) - part_lenght
         # Soma os bits overflow ao resultado sem o overflow
@@ -46,23 +49,34 @@ def complement_1(new_sum):
     return the_checksum
 
 #função de enviar
-def rdt_send(data, seqnumb, receiver):
+def rdt_send(data, seqnumb):
     data = data.encode()
     #primeiro fazendo o checksum dos dados
-    cks = checksum(data, len(data))
-    #criamos o pacot com o id, os dados e o checksum
-    sndpkt = [seqnumb, data, cks]
-    #envia o pacote pro destinatario
-    socket.sendto(sndpkt, receiver)
-
+    cks = checksum(data)
+    #chamar a função do complemento de 1
+    cpmt1 = complement_1(cks)
+    #criamos o pacote com o id, os dados e o checksum
+    pkt = [seqnumb, data, cpmt1]
+    sndpkt = str(pkt)
+    return sndpkt
 
 #função de receber
-def rdt_rcv():
-    rcvpkt, sender = socket.recvfrom(1024)
-    seqnumb = rcvpkt[0]
-    data = rcvpkt[1]
+def rdt_rcv(rcvpkt):
+    #recebe o pacote encapsulado com mensagem, numéro sequencial e checksum
+    rcvpkt = eval(rcvpkt)
+    #cria a variavel para os dados
+    data = rcvpkt[0]
+    #cria a variavel para o seqnumb
+    seqnumb = rcvpkt[1]
+    #checksum recebido com a mensagem
+    cks_rcv = rcvpkt[2]
+    #faz o checksum
     cks = checksum(data)
-    if cks == rcvpkt[2]:
-        rdt_send('ACK', seqnumb, sender)
+    #compara o checksum recebido com o calculado
+    if str(cks) == str(cks_rcv):
+        #envia os dados e o ack
+        return data, seqnumb, 'ACK'
+    #caso os checksum's sejam diferentes
     else:
-        rdt_send('NAK', seqnumb, sender)
+        #envia os dados e o nak
+        return data, seqnumb, 'NAK'
