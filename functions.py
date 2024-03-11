cf8e67f4b9f9cbb7d97ca9ec25c8479da83169c5
Maria Antônia <131059210+mams5@users.noreply.github.com>
2024-03-11 20:42:30 -0300

    outra forma de converter para bits

--- functions.py
+++ functions.py
@@ -3,14 +3,14 @@ import socket
 # Função checksum
 def checksum(data):
     # Transforma bytes em bits
-    message_bits = bin(int.from_bytes(data, byteorder='big'))[2:]  
+    message_bits = format(data[0], 'b')
     # Dividindo em 8 bits
     bytes_parts_list = []    # cria uma lista para as partes em 8 bits
-    part_length = 8
+    part_lenght = 8
     j = 8
     i = 0
     # Loop para dividir a mensagem em partes de 8 bits
-    while i <= len(message_bits):
+    while i < len(message_bits) :
         byte_part = message_bits[i:j]
         i = j
         j = j + 8
@@ -20,27 +20,27 @@ def checksum(data):
     # Soma as partes
     bits_sum = '00000000'
     for bit_part in bytes_parts_list:
-        # soma cada parte com a soma acumulada
+        # soma cada parte com a seguinte
         bits_sum = bin(int(bits_sum, 2) + int(bit_part, 2))[2:]
 
     # Adicionando o overflow
-    if len(bits_sum) > part_length:
+    if len(bits_sum) > part_lenght:
         # Calcula os bits overflow
-        exceed = len(bits_sum) - part_length
+        exceed = len(bits_sum) - part_lenght
         # Soma os bits overflow ao resultado sem o overflow
-        bits_sum = bin(int(bits_sum[exceed:], 2) + int(bits_sum[:exceed], 2))[2:]
-
+        bits_sum = bin(int(bits_sum[0:exceed], 2)+int(bits_sum[exceed:], 2))[2:]
     # Adiciona zeros à esquerda em caso de soma menor que 8 bits
-    if len(bits_sum) < part_length:
-        bits_sum = '0' * (part_length - len(bits_sum)) + bits_sum
-
+    if len(bits_sum) < part_lenght:
+        bits_sum = '0' * (part_lenght - len(bits_sum)) + bits_sum
+        
     final_checksum = complement_1(bits_sum)
     return final_checksum
 
 # Calculando o complemento de 1
-def complement_1(bits_sum):
+def complement_1(new_sum):
+  #  print(new_sum)
     the_checksum = ''
-    for bit in bits_sum:
+    for bit in new_sum:
         # Troca os 1 por 0
         if bit == '1':
             the_checksum += '0'
@@ -54,10 +54,8 @@ def make_pkt(data, seqnumb):
     data = data.encode()
     #primeiro fazendo o checksum dos dados
     cks = checksum(data)
-    #chamar a função do complemento de 1
-    cpmt1 = complement_1(cks)
     #criamos o pacote com o id, os dados e o checksum
-    pkt = [seqnumb, data, cpmt1]
+    pkt = [seqnumb, data, cks]
     sndpkt = str(pkt)
     return sndpkt
 
